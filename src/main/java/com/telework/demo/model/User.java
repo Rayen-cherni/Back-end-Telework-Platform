a16ffa89b0520ff62908adf4a830579ef71f4539
Rayen-cherni <rayen.cherni1160@gmail.com>
2021-07-03 02:32:41 +0200

    First commit

---
+++ src/main/java/com/telework/demo/model/User.java
@@ -0,0 +1,29 @@
+package com.telework.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "firstname")
+    private String firstname;
+
+    @Column(name = "lastname")
+    private String lastname;
+
+}
